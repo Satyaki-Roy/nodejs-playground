6482b6cd4ed5a4a67acc979db35206366f3e96b9
Satyaki Roy <satyaki.roy@morningstar.com>
2021-05-10 12:15:17 +0530

    Created express app with few routes

---
+++ express-essential/express-app/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import data from "./data/data.json";
+
+const app = express();
+const PORT = 3000;
+
+// middleware to serve static files
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.get("/items/:id", (req, res) => {
+  const id = Number(req.params.id);
+  res.send(data[id]);
+});
+
+// opening the port and listening to the requests
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+});

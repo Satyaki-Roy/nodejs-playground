d77a1c38e1ae5449fb1cda118faf2f92584ff00a
Satyaki Roy <satyaki.roy@morningstar.com>
2021-05-10 16:03:35 +0530

    express middlewares

--- express-essential/express-app/index.js
+++ express-essential/express-app/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import favicon from "serve-favicon";
+import path from "path";
+
 import data from "./data/data.json";
 
 const app = express();
@@ -7,6 +10,16 @@ const PORT = 3000;
 // middleware to serve static files
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
+
+// middleware to serve favicon
+console.log(__dirname);
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+// middleware to parse JSON data
+app.use(express.json());
+
+// middleware to parse URL encoded string
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json(data);
@@ -38,6 +51,24 @@ app
   .delete((req, res) => {
     res.send(`response from DELETE /items`);
   });
+
+app.post("/new-item", (req, res) => {
+  res.send(req.body);
+});
+
+app.post("/new-item-2", (req, res) => {
+  res.send(req.body);
+});
+
+app.get("/error", (req, res) => {
+  throw new Error();
+});
+
+// error handling middleware (should be at the last)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // opening the port and listening to the requests
 app.listen(PORT, () => {
